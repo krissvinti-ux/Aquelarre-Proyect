18d34f99efcc6e4ccf99801a43cc8a4264498030
Patricia <delitricia@hotmail.com>
2026-01-11 22:44:52 +0100

    add layout add router comment app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+// import { BrowserRouter } from "react-router-dom";
+// import Navbar from "./components/Navbar.jsx";
+// import Footer from "./components/Footer.jsx";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-black flex flex-col">
-        <Navbar />
-        <main className="flex-1 p-10 text-white">HERE ANIMATION</main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-}
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="min-h-screen bg-black flex flex-col">
+//         <Navbar />
+//         <main className="flex-1 p-10 text-white">HERE ANIMATION</main>
+//         <Footer />
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
