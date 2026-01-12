fa867a70eb665374ae345429de200dab9f069cc6
Jen Ceballos <jenniferceballoscastro@gmail.com>
2026-01-12 11:25:12 +0100

    conectar componente registrer y estilos finales

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import Registrer from "./pages/Registrer.jsx"; 
+import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-black flex flex-col">
+        {}
         <Navbar />
-        <main className="flex-1 p-10 text-white">HERE ANIMATION</main>
+
+        {}
+        <main className="flex-1 p-10 text-white flex justify-center items-center">
+          <Registrer />
+        </main>
+
+        {}
         <Footer />
       </div>
     </BrowserRouter>
